dc74cd8c146daa936b3df5ca8c44947dee69cb9f
Bhupi1306 <bhupesh0613@gmail.com>
2025-08-19 15:43:33 +0530

    Realtime Date and time and Greeting

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react"
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 export const DateTime = () => {
+
+    const [time, setTime] = useState(new Date());
+
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+        setTime(new Date());
+        }, 1000);
+    }, []);
+
     return (
         <div className="flex flex-col items-center justify-center text-white">
-            <p className="text-sm md:-mb-5 md:text-[1.5rem] md:tracking-wide  ">Monday, January 1</p>
+            <p className="text-sm md:-mb-5 md:text-[1.5rem] md:tracking-wide  ">{days[time.getDay()]}, {months[time.getMonth()]} {time.getDate()}</p>
 
 
             <p className="text-6xl
             md:text-[7rem]
-            xl:text-[8rem]">5:13</p>
+            xl:text-[8rem]">{time.getHours()<=12? time.getHours(): time.getHours() -12 }:{time.getMinutes()}</p>
         </div>
     )
 }

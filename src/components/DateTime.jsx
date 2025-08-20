c9581dca73acf972691826737fd92a3564e6725d
Bhupi1306 <bhupesh0613@gmail.com>
2025-08-20 16:51:25 +0530

    Added weatherMode

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-export const DateTime = () => {
+export const DateTime = ({weatherMode}) => {
 
     const [time, setTime] = useState(new Date());
 
@@ -17,10 +17,11 @@ export const DateTime = () => {
         <div className="flex flex-col items-center justify-center text-white">
             <p className="text-sm md:-mb-5 md:text-[1.5rem] md:tracking-wide  ">{days[time.getDay()]}, {months[time.getMonth()]} {time.getDate()}</p>
 
-
             <p className="text-6xl
             md:text-[7rem]
             xl:text-[8rem]">{time.getHours()<=12? time.getHours(): time.getHours() -12 }:{time.getMinutes()<10?"0"+time.getMinutes():time.getMinutes()}</p>
+            <p className={`${weatherMode? "": "hidden"} text-9xl`}>-</p>
         </div>
     )
 }
+

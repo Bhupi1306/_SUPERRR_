d0d47b5dab1c55387f01e0db5615a328f2e748bc
Bhupi1306 <bhupesh0613@gmail.com>
2025-08-20 16:52:44 +0530

    Make Todolist and its sidebar

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react"
+import { Task } from "./Task"
+
 export const Todo = () => {
+    const [expand, setExpand] = useState(false)
+
     return (
         <>
-            <div className="absolute bottom-20 right-10 bg-gray-800/50 backdrop-blur-md 
-                        text-white rounded-2xl p-4 w-64 shadow-lg">
-                <h3 className="mb-2 font-semibold">Todo</h3>
-                <ul className="space-y-2 text-sm">
-                    <li className="flex items-center gap-2"><input type="checkbox"/> Task 1</li>
-                    <li className="flex items-center gap-2"><input type="checkbox"/> Task 2</li>
-                    <li className="flex items-center gap-2"><input type="checkbox"/> Task 3</li>
-                    <li className="text-xs opacity-70">...view more</li>
-                </ul>
+            <div 
+                className={`absolute  bg-white/20 backdrop-blur-md rounded overflow-auto text-white p-4 w-64 shadow-lg 
+                ${expand? "h-full bottom-0 right-0 w-[20rem] pt-8 pl-5" :"bottom-10 right-10 rounded-md"}`
+            }>
+                <h3 className="mb-2 font-semibold text-[1.1rem]">Todo</h3>
+
+                {expand && (
+                    <>
+                    <input type="text" placeholder="+ Add task" className="w-[13rem] mt-2 p-1 focus:outline-none text-sm" />
+                    <hr className="mb-3"/>
+                    </>
+                )}
+
+                <Task/>
+                <Task/>
+                <Task/>
+
+                {!expand && (<p className="text-[0.875rem] leading-[1rem] mt-3">...view more</p>)}
             </div>
         </>
     )
 }
-
-

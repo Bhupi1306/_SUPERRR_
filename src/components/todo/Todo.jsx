795525d0c7eb5b7f30a5865feb5dcca26925d37d
farthestmage <arnavkamboj511@gmail.com>
2025-08-23 15:48:10 +0530

    Todo out click

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,30 +1,51 @@
-import { useState } from "react"
+import { useState,useRef,useEffect } from "react"
 import { Task } from "./Task"
 import { useTasks } from "./useTask"
 
 export const Todo = () => {
     const [expand, setExpand] = useState(false)
     const [tasks, setTasks] = useTasks()
+    const menuRef = useRef(null)
 
+    
+    useEffect(() => {
+         
+    function handleClickOutside(evt) {
+      if (menuRef.current && !menuRef.current.contains(evt.target)) {
+        console.log("Clicked outside, collapsing menu")
+        setExpand(false)
+      }
+    }
 
-     const toggleTask = (id) => {
+    if (expand) {
+      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("touchstart", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside)
+      document.removeEventListener("touchstart", handleClickOutside)
+    }
+    }, [expand]) 
+    
+    const toggleTask = (id) => {
     setTasks((prevTasks) =>
       prevTasks.map((task) =>
         task.id === id ? { ...task, done: !task.done } : task
-      )
-    )
-  }
-  const editTask = (id, newName) => {
+                    )
+            )
+        }
+    const editTask = (id, newName) => {
     setTasks((prev) =>
       prev.map((task) =>
         task.id === id ? { ...task, taskName: newName } : task
-      )
-    )
-  }
- const deleteTask = (id) => {
+            )
+        )
+    }
+    const deleteTask = (id) => {
     setTasks((prev) => prev.filter((task) => task.id !== id))
-  }
-  const addTask = (taskName) => {
+    }
+    const addTask = (taskName) => {
     const newTask = {
       id: Date.now(),
         taskName: taskName,
@@ -33,17 +54,19 @@ export const Todo = () => {
 
     return (
         <>
-            <div 
+            <div ref={menuRef}
                 className={`absolute  bg-white/10 backdrop-blur-sm rounded overflow-auto text-white p-4 w-64 shadow-lg 
                 ${expand? "h-full bottom-0 right-0 w-[20rem] pt-8 pl-5" :"bottom-10 right-10 rounded-md"}`
             }>
                 <h3 className="mb-2 font-semibold text-[1.1rem]">Todo</h3>
 
                 {expand && (
+                    
                     <>
                     <input  onKeyUp={e => e.key === "Enter" && addTask(e.currentTarget.value)} type="text" placeholder="+ Add task" className="w-[13rem] mt-2 p-1 focus:outline-none text-sm" />
                     <hr className="mb-3"/>
                     </>
+                     
                 )}
 
               {tasks.map((task) => (

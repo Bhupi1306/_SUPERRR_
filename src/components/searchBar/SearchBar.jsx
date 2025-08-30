da8d656427727bf330fff9fdb1882f172dc7fe95
Bhupi1306 <bhupesh0613@gmail.com>
2025-08-30 11:34:33 +0530

    Drop down for search engine but not functional

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,15 +1,29 @@
 import { useEffect, useRef, useState } from "react"
 import SearchIcon from "../svg/SearchIcon"
+import GoogleLogo from "../svg/Google"
+import DuckLogo from "../svg/DuckDuckGo"
 
 export const queryToken = "{{query}}"
+
+const searchEngines = [
+    {
+        "id":"Google",
+        "engine":"www.google.com/search",
+        "icon" : <GoogleLogo size={16}/>
+    },
+    {
+        "id":"DuckDuckGo",
+        "engine":"www.duckduckgo.com/search",
+        "icon" : <DuckLogo size={16}/>
+    },
+]
 
 export const SearchBar = () => {
 
     const[searching, setSearching] = useState(false)
-    const[engine, setEngine] = useState("www.google.com/search")
+    const[engine, setEngine] = useState(searchEngines[0])
     const searchRef = useRef(null)
     const inputRef = useRef(null)
-
 
     // Handling click outside the search bar
     useEffect(() => {
@@ -60,10 +74,10 @@ export const SearchBar = () => {
 
     // Redirecting user to query
     const redirectToSearch = (query) => {  
-          if (!engine.includes(queryToken)) {
-            window.location.href = `https://${engine}?q=${encodeURIComponent(query)}`
+          if (!engine.engine.includes(queryToken)) {
+            window.location.href = `https://${engine.engine}?q=${encodeURIComponent(query)}`
           } else {
-            window.location.href = engine.replace(queryToken, encodeURIComponent(query))
+            window.location.href = engine.engine.replace(queryToken, encodeURIComponent(query))
           }
       }
     
@@ -73,10 +87,16 @@ export const SearchBar = () => {
             {/* Actual Search bar */}
             {searching && (<div className="w-screen h-screen absolute left-0 top-0 grid grid-rows-4 place-items-center bg-amber-50/20 backdrop-blur-sm z-100">
                 <div ref={searchRef}  
-                    className="row-start-2 flex justify-center items-center rounded-md bg-gray-50/85  w-[40rem] relative overflow-hidden"
+                    className="row-start-2 flex justify-center items-center rounded-md bg-gray-50/85  w-[40rem] relative"
                 >
                     
-                    <div className="px-4 py-2  border-r-1 border-gray-500/20 absolute left-0">G</div>
+                    <div className="px-4 py-2  border-r-1 border-gray-500/20 absolute left-0">{engine.icon}</div>
+                    <div className="bg-gray-50/70  absolute top-full left-0  p-3 rounded-md mt-2">
+                        {searchEngines.map(search => (<div key={search.id} className="flex space-x-5 mb-1 items-center  text-[0.9rem] justify-between text-left text-black/85">
+                            <div>{search.icon}</div>
+                            <div id={search.id} className="w-full" onClick={(e)=> {console.log("Clicked",e.target.id)}}>{search.id}</div>
+                        </div>))}
+                    </div>
 
                     <div className="absolute right-3"><SearchIcon size={22} color="#00000055"/></div>
 
